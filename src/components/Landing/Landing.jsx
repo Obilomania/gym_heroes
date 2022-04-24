7947b75680a5e5f812120913db53a1c9a50cabca
obilomania <iloanyaobinna@gmail.com>
2022-04-24 22:50:30 +0100

    Navigation complate and responsive

---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Landing.css"
+// import bg from "../../../assets/bg.jpg"
+
+const Landing = () => {
+  return (
+    <div className='header'>
+      <div className="content">
+        <h3>HAVE</h3>
+        <h1>SUMMER BODY</h1>
+        <h3>EVERY</h3>
+        <h1>SEASON</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Landing
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import "./Navigation.css"
+
+const Navigation = () => {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click)
+    
+    return (
+        <>
+            <nav>
+                <h1>GYM <span>Heroes</span> </h1>
+                <ul className={click ? "navLinks active" : "navLinks"}>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Service</a></li>
+                    <li><a href="#">Gym Plans</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <div className="hamburger" onClick={handleClick}>
+                    {click ? (<FaTimes size={25} style={{ color: 'rgb(231, 10, 139)'}} />) : (<FaBars size={25} style={{ color: 'rgb(231, 10, 139)' }} />)}
+
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navigation
